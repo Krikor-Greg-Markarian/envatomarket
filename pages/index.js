caffa881b9cbadbb78c2515b9bf78561e51bfdb3
Greg <greg_markarian@hotmail.com>
2021-10-28 13:56:14 +0300

    WIP

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,34 @@
-
 import cn from "classnames";
 import axios from "axios";
+import BlackNavbar from "../src/components/BlackNavbar";
+import BlueNavbar from "../src/components/BlueNavbar";
 
 export default function Home({}) {
   return (
     <div>
-      
+      {/* //here is the first navbar black navbar */}
+      <section>
+        <BlackNavbar />
+      </section>
+ {/* //here is the second navbar blue navbar */}
+      <section>
+        <BlueNavbar />
+      </section>
     </div>
   );
 }
 
+// // export async function getServerSideProps(context) {
+// //   const houzezResponse = await axios.get(
+// //     "https://8b7d0628-947c-4c7a-b442-32304eac7557.mock.pstmn.io/houzez"
+// //   );
 
-export async function getServerSideProps(context) {
-  const houzezResponse = await axios.get(
-    "https://ea50086b-af54-4718-ab27-001ee37d4e28.mock.pstmn.io/houzez"
-  );
-  
+// //   const houzez = houzezResponse.data.data || [];
 
-  
+// //   return {
+// //     props: {
+// //       houzez: houzez,
 
-  
-
-  const houzez = houzezResponse.data.data || [];
-  
-  return {
-    props: {
-      houzez: houzez,
-     
-    }, // will be passed to the page component as props
-  };
-}
+// //     }, // will be passed to the page component as props
+//   };
+// }
