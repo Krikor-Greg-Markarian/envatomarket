8ba2e749875cd59d468216557e9af4578708766a
Greg <greg_markarian@hotmail.com>
2021-10-28 13:56:01 +0300

    ADD: component blue navbar

---
+++ src/components/BlueNavbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import cn from "classnames";
+
+function BlueNavbar(props) {
+  return (
+    <div className={cn("bg-blue-900 p-2")}>
+      <div className={cn("inline text-center text-yellow-500")}>
+        <p>
+          Due to the COVID 19 epidemic, orders may be processed with a slight
+          delay
+          <span className={cn(" text-white float-right pr-5")}>A</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default BlueNavbar;
