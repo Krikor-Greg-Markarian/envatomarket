1067228ec4e9f1eb9310fd146100b254495e7c58
Greg <greg_markarian@hotmail.com>
2021-11-01 17:38:17 +0200

    added product sections

--- src/components/Products.js
+++ src/components/Products.js
@@ -72,6 +72,336 @@ function Products(props) {
             </Link>
           </li>
         </div>
+        <div className={cn("col-span-1")}>
+          <p className={cn("text-blue-800 font-bold")}>Higiene</p>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-3 transition duration-500 ease-in-out hover:text-yellow-600 hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Disponsable products</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Face masks</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Gloves</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Protect covers</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Sterilization</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Surgical covers</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Uniforms</a>
+            </Link>
+          </li>
+        </div>
+        <div className={cn("col-span-1")}>
+          <p className={cn("text-blue-800 font-bold")}>Laboratory</p>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-3 transition duration-500 ease-in-out hover:text-yellow-600 hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Devices</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Diagnostic tests</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Disinfectants</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Dyes</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Pipettes</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Test-tubes</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Tubes</a>
+            </Link>
+          </li>
+        </div>
+        <div className={cn("col-span-1")}>
+          <p className={cn("text-blue-800 font-bold")}>Tools</p>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-3 transition duration-500 ease-in-out hover:text-yellow-600 hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Accessories Tools</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Blood pressure</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Capsules</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Dental</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Micrscope</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Pressure</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Sugar level</a>
+            </Link>
+          </li>
+        </div>
+        <div className={cn("col-span-1")}>
+          <p className={cn("text-blue-800 font-bold")}>Diagnosis</p>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-3 transition duration-500 ease-in-out hover:text-yellow-600 hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Biopsy tools</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Endoscopes</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Monitoring</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Otoscopes</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Oxygen concentrators</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Tables and assistants</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Thermometer</a>
+            </Link>
+          </li>
+        </div>
+        <div className={cn("col-span-1")}>
+          <p className={cn("text-blue-800 font-bold")}>Equipment</p>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-3 transition duration-500 ease-in-out hover:text-yellow-600 hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Blades</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Education</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Germicidal lamps</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Heart Health</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Infusion stands</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Lighting</a>
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "text-blue-800 list-none pt-2 hover:text-yellow-600 transition duration-500 ease-in-out hover:underline"
+            )}
+          >
+            <Link href="">
+              <a href="">Machines</a>
+            </Link>
+          </li>
+        </div>
       </div>
     </div>
   );
