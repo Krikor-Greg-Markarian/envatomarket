e494310581ef021d1694a0c20877368850464faa
Greg <greg_markarian@hotmail.com>
2021-11-02 17:22:26 +0200

    WIP

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,13 @@ import {
   Fa500Px,
   FaAccessibleIcon,
   FaInstagram,
+  FaFacebookF,
+  FaYoutube,
+  FaPinterestP,
+  FaLinkedinIn,
+  FaPhone,
+  FaRegEnvelope,
+  FaDirections,
 } from "react-icons/fa";
 import BoxComponent from "../src/components/BoxComponent";
 import MostPopularCategories from "../src/components/MostPopularCategories";
@@ -20,6 +27,8 @@ import GetRidOfBecteria from "../src/components/GetRidOfBecteria";
 import LatestReviews from "../src/components/LatestReviews";
 import React, { useState } from "react";
 import MoneyBack from "../src/components/MoneyBack";
+import { GiHeartPlus } from "react-icons/gi";
+import Link from "next/dist/client/link";
 
 export default function Home({
   boxComponents,
@@ -28,6 +37,7 @@ export default function Home({
   bestDealsOfTheWeek,
   getRidOfBakteria,
   latestReviews,
+  moneyBack,
 }) {
   const [isShown, setIsShown] = useState(false);
   return (
@@ -50,44 +60,83 @@ export default function Home({
         <LastNavbar />
       </section>
       {/* here is the section were antibacterial media masks are marketed */}
-      <section className={cn("flex justify-center items-center")}>
-        <div className={cn("grid grid-cols-2")}>
-          <div className={cn("col-span-1")}>
-            <p className={cn("text-blue-900 text-5xl font-bold")}>
-              Antibacterial Media Mask
-            </p>
-            <p className={cn("text-2xl pt-4 pb-4 text-blue-900")}>
-              Just a few seconds to measure your body temperature.
-            </p>
-            <div className={cn("flex")}>
-              <div className={cn("flex-shrink")}>
-                <Fa500Px className={cn("text-gray-500 inline")} />
+      <div className={cn("bg-gray-200 py-16")}>
+        <section
+          className={cn(
+            "flex justify-center items-center container mx-auto w-8/12"
+          )}
+        >
+          <div className={cn("grid grid-cols-2 ")}>
+            <div className={cn("col-span-1 w-8/12 ")}>
+              <p className={cn("text-blue-900 text-5xl font-bold")}>
+                Antibacterial Media Mask
+              </p>
+              <p className={cn("text-2xl pt-6 pb-6 text-blue-900")}>
+                Just a few seconds to measure your body temperature.
+              </p>
+              <div className={cn("flex")}>
+                <div className={cn("flex-shrink")}>
+                  <Fa500Px className={cn("text-gray-500 inline")} />
+                </div>
+                <div className={cn("flex-shrink pl-2")}>
+                  <p className={cn("inline font-bold text-gray-500")}>
+                    Anti-Bacterial
+                  </p>
+                </div>
+                <div className={cn("flex-shrink pl-12")}>
+                  <FaAccessibleIcon className={cn("text-gray-500 inline")} />
+                </div>
+                <div className={cn("flex-shrink pl-2")}>
+                  <p className={cn("inline font-bold text-gray-500")}>
+                    Anti-Virus
+                  </p>
+                </div>
               </div>
-              <div className={cn("flex-shrink pl-2")}>
-                <p className={cn("inline font-bold text-gray-500")}>
-                  Anti-Bacterial
-                </p>
+              <div className={cn("pt-6")}>
+                <Button
+                  className={cn(
+                    "bg-yellow-500 rounded-full px-14 py-4 text-center text-white font-bold hover:bg-white hover:text-yellow-500 transition duration-500 ease-in-out"
+                  )}
+                  buttonName={"Shop now"}
+                />
               </div>
-              <div className={cn("flex-shrink pl-12")}>
-                <FaAccessibleIcon className={cn("text-gray-500 inline")} />
-              </div>
-              <div className={cn("flex-shrink pl-2")}>
-                <p className={cn("inline font-bold text-gray-500")}>
-                  Anti-Virus
-                </p>
+            </div>
+            <div className={cn("col-span-1")}>
+              <div className={cn("mask relative")}>
+                <div className={cn("abolute")}>
+                  <div
+                    className={cn(
+                      "bg-yellow-400 absolute top-0 right-24 w-32 h-32 rounded-full"
+                    )}
+                  >
+                    <p
+                      className={cn(
+                        "text-center pt-8 text-xl font-bold text-blue-900"
+                      )}
+                    >
+                      only
+                    </p>
+                    <p
+                      className={cn(
+                        "text-center text-4xl font-bold text-blue-900"
+                      )}
+                    >
+                      $25
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div className={cn("mask")}>asdasd</div>
-        </div>
-      </section>
+        </section>
+      </div>
       {/* 3 box sections  */}
       <section>
         <div className={cn("container mx-auto")}>
           <div className={cn("flex justify-center items-center")}>
             <div className={cn("grid grid-cols-3 gap-8")}>
               {boxComponents.map((item, idx) => (
-                <div className={cn("col-span-1")}>
+                <div className={cn("col-span-1 pt-8")}>
                   <BoxComponent
                     imageUrl={item.imageUrl}
                     isNew={item.isNew}
@@ -146,7 +195,7 @@ export default function Home({
             Latest products
           </p>
           <div className={cn("flex justify-center items-center")}>
-            <div className={cn("grid grid-cols-4")}>
+            <div className={cn("grid grid-cols-5")}>
               {latestProduct.map((item, idx) => (
                 <div className={cn("col-span-1")}>
                   <LatestProducts
@@ -535,8 +584,283 @@ $20 discount for your first order */}
 
       {/* Money back section and more */}
 
+      <section className= {cn("pt-12 pb-12")}>
+        <div className={cn("flex justify-center items-center")}>
+          <div className={cn("grid grid-cols-3")}>
+            {moneyBack.map((item, idx) => (
+              <div className={cn("col-span-1")}>
+                <MoneyBack
+                  isWallet={item.isWallet}
+                  title={item.title}
+                  isPackage={item.isPackage}
+                  isDelivery={item.isDelivery}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      {/* Footer before lat section */}
+
+      <section className={cn("pt-4")}>
+        <div className={cn("container mx-auto")}>
+          <div className={cn("grid grid-cols-6 ")}>
+            <div className={cn("col-span-1")}>
+              <div className={cn("border-r-2")}>
+                <div className={cn("flex pt-10 ")}>
+                  <div className={cn("flex-shrink")}>
+                    <GiHeartPlus
+                      className={cn("text-blue-300 text-3xl inline ")}
+                    />
+                  </div>
+                  <div className={cn("flex-shrink pl-2")}>
+                    <p className={cn("text-blue-800 text-2xl font-bold")}>
+                      MYMEDI
+                    </p>
+                  </div>
+                </div>
+                <p className={cn("text-gray-500 pt-4 pb-4")}>
+                  1487 Rocky Horse Carrefour Arlington, TX 16819
+                </p>
+                <p className={cn("underline text-gray-500 text-sm")}>
+                  Show on map
+                </p>
+                <div className={cn("flex pt-4")}>
+                  <div className={cn("flex-shrink")}>
+                    <FaFacebookF
+                      className={cn(
+                        "text-gray-500 text-lg cursor-pointer hover:text-blue-900"
+                      )}
+                    />
+                  </div>
+                  <div className={cn("flex-shrink pl-3")}>
+                    <FaInstagram
+                      className={cn(
+                        "text-gray-500 text-lg cursor-pointer hover:text-blue-900"
+                      )}
+                    />
+                  </div>
+                  <div className={cn("flex-shrink pl-3")}>
+                    <FaYoutube
+                      className={cn(
+                        "text-gray-500 text-lg cursor-pointer hover:text-blue-900"
+                      )}
+                    />
+                  </div>
+                  <div className={cn("flex-shrink pl-3")}>
+                    <FaPinterestP
+                      className={cn(
+                        "text-gray-500 text-lg cursor-pointer hover:text-blue-900"
+                      )}
+                    />
+                  </div>
+                  <div className={cn("flex-shrink pl-3")}>
+                    <FaLinkedinIn
+                      className={cn(
+                        "text-gray-500 cursor-pointer hover:text-blue-900"
+                      )}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className={cn("col-span-2 pl-3")}>
+              <p className={cn("text-blue-900 font-bold pl-8 pb-4")}>
+                Need help
+              </p>
+              <div className={cn("flex pl-3")}>
+                <div className={cn("flex-shrink ")}>
+                  <FaPhone className={cn("text-yellow-500 text-xl inline")} />
+                </div>
+                <div className={cn("flex-shrink")}>
+                  <p className={cn("text-blue-900 font-bold inline text-2xl")}>
+                    0020 500 - MYMEDI - 000
+                  </p>
+                  <p className={cn("text-gray-500 pt-4 text-sm")}>
+                    Monday - Friday: 9:00-20:00
+                  </p>
+                  <p className={cn("text-gray-500 pt-2 text-sm")}>
+                    Saturday: 11:00 - 15:00
+                  </p>
+                </div>
+              </div>
+              <hr className={cn("mt-4 ml-12 w-9/12")}></hr>
+              <div className={cn("flex pl-8 pt-3")}>
+                <div className={cn("flex-shrink pt-1")}>
+                  <FaRegEnvelope className={cn("text-blue-900")} />
+                </div>
+                <div className={cn("flex-shrink pl-4")}>
+                  <p className={cn("text-blue-900 font-bold")}>
+                    Contact@example.com
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={cn("col-span-1")}>
+              <p className={cn("text-blue-900 font-bold")}>information</p>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-4"
+                )}
+              >
+                <Link href="">
+                  <a href="">About us</a>
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-2"
+                )}
+              >
+                <Link href="">
+                  <a href="">Delivery information</a>
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-2"
+                )}
+              >
+                <Link href="">
+                  <a href="">Privacy Policy</a>
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-2"
+                )}
+              >
+                <Link href="">
+                  <a href="">Sales</a>
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-2"
+                )}
+              >
+                <Link href="">
+                  <a href="">Terms &amp; Conditions</a>
+                </Link>
+              </li>
+            </div>
+            <div className={cn("col-span-1")}>
+              <p className={cn("text-blue-900 font-bold")}>Account</p>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-4"
+                )}
+              >
+                <Link href="">
+                  <a href="">My account</a>
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-2"
+                )}
+              >
+                <Link href="">
+                  <a href="">My orders</a>
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-2"
+                )}
+              >
+                <Link href="">
+                  <a href="">Returns</a>
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-2"
+                )}
+              >
+                <Link href="">
+                  <a href="">Shipping</a>
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-2"
+                )}
+              >
+                <Link href="">
+                  <a href="">Wishlist</a>
+                </Link>
+              </li>
+            </div>
+            <div className={cn("col-span-1")}>
+              <p className={cn("text-blue-900 font-bold")}>Store</p>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-4"
+                )}
+              >
+                <Link href="">
+                  <a href="">Affiliate</a>
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-2"
+                )}
+              >
+                <Link href="">
+                  <a href="">Bestsellers</a>
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-2"
+                )}
+              >
+                <Link href="">
+                  <a href="">Discount</a>
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-2"
+                )}
+              >
+                <Link href="">
+                  <a href="">Latest products</a>
+                </Link>
+              </li>
+              <li
+                className={cn(
+                  "text-blue-900 list-none hover:text-yellow-500 transition duration-500 ease-in-out pt-2"
+                )}
+              >
+                <Link href="">
+                  <a href="">Sale</a>
+                </Link>
+              </li>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* footer last section */}
+
       <section>
-        <MoneyBack />
+        <div className={cn("container mx-auto pt-8")}>
+          <div className={cn("p-4 border-t-2")}>
+            <p className={cn("text-blue-900 inline pt-2")}>
+              Copyright &copy; 2020 Mymedi. All Rights Reserved
+            </p>
+            <img
+              className={cn("inline float-right")}
+              src="https://mymedi-be87.kxcdn.com/mymedi/wp-content/uploads/2020/08/payment.png"
+              alt=""
+            />
+          </div>
+        </div>
       </section>
     </div>
   );
@@ -566,12 +890,17 @@ export async function getServerSideProps(context) {
     "https://8b7d0628-947c-4c7a-b442-32304eac7557.mock.pstmn.io/latestReviews"
   );
 
+  const moneyBackResponse = await axios.get(
+    "https://8b7d0628-947c-4c7a-b442-32304eac7557.mock.pstmn.io/moneyBack"
+  );
+
   const boxComponents = boxComponentsResponse.data.data || [];
   const mostPopularCategories = mostPopularCategoriesResponse.data.data || [];
   const latestProduct = latestProductResponse.data.data || [];
   const bestDealsOfTheWeek = bestDealsOfTheWeekResponse.data.data || [];
   const getRidOfBakteria = getRidOfBakteriaResponse.data.data || [];
   const latestReviews = latestReviewsResponse.data.data || [];
+  const moneyBack = moneyBackResponse.data.data || [];
 
   return {
     props: {
@@ -581,6 +910,7 @@ export async function getServerSideProps(context) {
       bestDealsOfTheWeek: bestDealsOfTheWeek,
       getRidOfBakteria: getRidOfBakteria,
       latestReviews: latestReviews,
+      moneyBack: moneyBack,
     }, // will be passed to the page component as props
   };
 }
