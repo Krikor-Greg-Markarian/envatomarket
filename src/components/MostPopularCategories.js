5b90f609b500287e41eca7b739e433e45feb9baa
Greg <greg_markarian@hotmail.com>
2021-11-02 17:23:30 +0200

    ADD: image props

--- src/components/MostPopularCategories.js
+++ src/components/MostPopularCategories.js
@@ -5,24 +5,17 @@ import Button from "./Button";
 function MostPopularCategories(props) {
   return (
     <div className={cn("bg-gray-200 p-5")}>
-      <div
-        style={{
-          width: "230px",
-          height: "260px",
-          // backgroundImage: `url(${props.imageUrl})`,
-        }}
-      >
-        <p className={cn("text-blue-900 text-xl font-bold pt-3")}>
-          {props.title}
-        </p>
-        <div className={cn("pt-3")}>
-          <Button
-            className={cn(
-              "bg-white text-center font-bold rounded-full text-blue-900 px-4 py-1"
-            )}
-            buttonName={props.moreButton}
-          />
-        </div>
+      <p className={cn("text-blue-900 text-xl font-bold pt-3")}>
+        {props.title}
+      </p>
+      <div className={cn("pt-3")}>
+        <Button
+          className={cn(
+            "bg-white text-center font-bold rounded-full text-blue-900 px-4 py-1"
+          )}
+          buttonName={props.moreButton}
+        />
+        <img className={cn("w-60 h-60")} src={props.imageUrl} alt="" />
       </div>
     </div>
   );
